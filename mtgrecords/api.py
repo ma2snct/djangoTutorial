afa8207b6b4ba8c15cb7522047e5f5a65da98619
ma2snct <knightmarespark@gmail.com>
2018-01-01 19:04:45 +0900

    twitter開通

---
+++ mtgrecords/api.py
@@ -0,0 +1,4 @@
+token = "1681008974-DAyNiMPHQRKIIcf7voeGTGlGSDSwEewinhQsmKm"
+token_secret = "RWFxlmC7Yeim7L8w5yKyVjv0wPKvk2I6NGM8Db1hq0peE"
+consumer_key = "HpBeeQCSoFeEArPfl30ap8f0E"
+consumer_secret = "uEcKiac7UGaJAzesIxeWxBlr5rJdC7BA08tNZHgRg1Ad1KqIxi"
